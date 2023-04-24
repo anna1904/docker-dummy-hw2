59c044289bf83ac730826c739af33d6f83d843e7
Anna Konovalenko <annakonovlnko@gmail.com>
2023-04-24 23:19:51 +0100

    Changed upload, get

--- Data storage/CRUD-client-minio.py
+++ Data storage/CRUD-client-minio.py
@@ -1,23 +1,53 @@
 from minio import Minio
+from pathlib import Path
 import unittest
+
+ACCESS_KEY = "minioadmin"
+SECRET_KEY = "minioadmin"
+ENDPOINT = "0.0.0.0:9000"
 
 class Minio_client():
     def __init__(self):
         try:
-            self.minioClient = Minio("hostPath", access_key = "db_user", secret_key = "db_key", secure = False)
+            self.minioClient = Minio(ENDPOINT, access_key = ACCESS_KEY, secret_key = SECRET_KEY, secure = False)
         except:
             print("Not able to connect minio / {}".format(Exception))
 
-    def create_object(self, bucket_name, object_name):
+    def upload_object(self, bucket_name, file_path: Path):
         try:
             bucket = self.minioClient.bucket_exists(bucket_name)
             if bucket:
-                self.minioClient.put_object(bucket_name, object_name, data=object_name)
+                self.minioClient.fput_object(bucket_name, file_path.name, file_path)
                 print("Dataset/Model has been sucessfully saved in {}".format(bucket_name))
+        except:
+            print('Cannot upload an object')
+    def get_object(self, bucket_name, file_path: Path):
+        try:
+            bucket = self.minioClient.bucket_exists(bucket_name)
+            if bucket:
+                self.minioClient.fget_object(bucket_name, file_path.name, file_path)
+                print("Dataset/Model has been sucessfully saved in {}".format(bucket_name))
+        except:
+            print('Cannot upload an object')
+
+    # def delete_object(self, bucket_name, file_path: Path):
+    #     try:
+    #         bucket = self.minioClient.bucket_exists(bucket_name)
+    #         if bucket:
+    #             self.minioClient.fput_object(bucket_name, file_path.name, file_path)
+    #             print("Dataset/Model has been sucessfully saved in {}".format(bucket_name))
+    #     except:
+    #         print('Cannot upload an object')
+    # def update_object(self, bucket_name, file_path: Path):
+    #     try:
+    #         bucket = self.minioClient.bucket_exists(bucket_name)
+    #         if bucket:
+    #             self.minioClient.fput_object(bucket_name, file_path.name, file_path)
+    #             print("Dataset/Model has been sucessfully saved in {}".format(bucket_name))
+    #     except:
+    #         print('Cannot upload an object')
 
 
-class UnitTests(unittest.TestCase)
-    __init__(self):
 
 
 
