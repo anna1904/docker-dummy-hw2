3b7fed3429a8f82df95b158e8f04c28fb17b7c54
Anna Konovalenko <annakonovlnko@gmail.com>
2023-05-23 14:09:34 +0200

    Fix with fixtures and remove try-catch in client

--- Data_storage/client_minio.py
+++ Data_storage/client_minio.py
@@ -6,59 +6,35 @@ ACCESS_KEY = "minioadmin"
 SECRET_KEY = "minioadmin"
 ENDPOINT = "0.0.0.0:9000"
 
-class Minio_client():
+
+class MinioClient:
     def __init__(self):
-        try:
-            self.minioClient = Minio(ENDPOINT, access_key = ACCESS_KEY, secret_key = SECRET_KEY, secure = False)
-        except:
-            print("Not able to connect minio / {}".format(Exception))
+        self.minioClient: Minio = Minio(ENDPOINT, access_key=ACCESS_KEY, secret_key=SECRET_KEY, secure=False)
 
-    def upload_object(self, bucket_name, object_name, file_path: Path):
-        try:
-            bucket = self.minioClient.bucket_exists(bucket_name)
-            if bucket:
-                self.minioClient.fput_object(bucket_name, object_name, file_path)
-                print("File has been sucessfully saved in {}".format(bucket_name))
-        except:
-            print("Not able to upload file / {}".format(Exception))
-    def get_object(self, bucket_name, object_name, file_path: Path):
-        try:
-            bucket = self.minioClient.bucket_exists(bucket_name)
-            if bucket:
-                result = self.minioClient.fget_object(bucket_name, object_name, file_path)
-                print("res:", result)
-                print(f"File {result.object_name} has been sucessfully get from {bucket_name}")
-                return result
-        except Exception as e:
-            print("Cannot upload an object/ {}".format(e))
-    def remove_object(self, bucket_name, object_name):
-        try:
-            bucket = self.minioClient.bucket_exists(bucket_name)
-            if bucket:
-                self.minioClient.remove_object(bucket_name, object_name)
-                print("File has been sucessfully deleted from {}".format(bucket_name))
-        except:
-            print("Cannot delete an object/ {}".format(Exception))
+    def upload_object(self, bucket_name: str, object_name: str, file_path: Path) -> None:
+        bucket = self.minioClient.bucket_exists(bucket_name)
+        if bucket:
+            self.minioClient.fput_object(bucket_name, object_name, file_path)
+            print("File has been successfully saved in {}".format(bucket_name))
 
-    #As objects are immutable in S3, I first delete the object and upload the new version with the same name
-    def update_object(self, bucket_name, object_name, file_path):
-        try:
-            bucket = self.minioClient.bucket_exists(bucket_name)
-            if bucket:
-                self.minioClient.remove_object(bucket_name, object_name)
-                self.minioClient.fput_object(bucket_name, object_name, file_path)
-                print(f"File {object_name} has been sucessfully updated")
-        except:
-            print("Cannot update an object/ {}".format(Exception))
-c = Minio_client()
-c.upload_object('mybucket','qw', 'qw.txt' )
-c.get_object('mybucket','qw', 'qw.txt' )
+    def get_object(self, bucket_name: str, object_name: str, file_path: Path):
+        bucket = self.minioClient.bucket_exists(bucket_name)
+        if bucket:
+            result = self.minioClient.fget_object(bucket_name, object_name, file_path)
+            print("res:", result)
+            print(f"File {result.object_name} has been successfully get from {bucket_name}")
+            return result
 
+    def remove_object(self, bucket_name: str, object_name: str) -> None:
+        bucket = self.minioClient.bucket_exists(bucket_name)
+        if bucket:
+            self.minioClient.remove_object(bucket_name, object_name)
+            print("File has been successfully deleted from {}".format(bucket_name))
 
-
-
-
-
-
-
-
+    # As objects are immutable in S3, I first delete the object and upload the new version with the same name
+    def update_object(self, bucket_name: str, object_name: str, file_path: Path) -> None:
+        bucket = self.minioClient.bucket_exists(bucket_name)
+        if bucket:
+            self.minioClient.remove_object(bucket_name, object_name)
+            self.minioClient.fput_object(bucket_name, object_name, file_path)
+            print(f"File {object_name} has been successfully updated")
--- Data_storage/tests-minio-client.py
+++ Data_storage/tests-minio-client.py
@@ -2,37 +2,43 @@ import os
 import unittest
 from pathlib import Path
 from unittest.mock import MagicMock
+import pytest
 
 from minio import Minio
 
-from client_minio import Minio_client, ACCESS_KEY, SECRET_KEY, ENDPOINT
+from client_minio import MinioClient
+
+
+@pytest.fixture()
+def bucket_name() -> str:
+    return "test-bucket"
+
+
+@pytest.fixture()
+def object_name() -> str:
+    return "test-object"
+
+
+@pytest.fixture()
+def file_path() -> Path:
+    return Path("test-file.txt")
+
 
 class TestMinioClient(unittest.TestCase):
-
-    def setUp(self):
+    def setUp(self) -> None:
         # Set up a mock Minio client object for testing
         self.mock_client = MagicMock(spec=Minio)
-        self.client = Minio_client()
+        self.client = MinioClient()
         self.client.minioClient = self.mock_client
 
-    def test_upload_object(self):
-        # Set up test data
-        bucket_name = "test-bucket"
-        object_name = "test-object"
-        file_path = Path("test-file.txt")
-
+    def test_upload_object(self) -> None:
         # Test successful upload
         self.mock_client.bucket_exists.return_value = True
         self.client.upload_object(bucket_name, object_name, file_path)
         self.mock_client.fput_object.assert_called_once_with(
             bucket_name, object_name, file_path)
 
-    def test_get_object(self):
-        # Set up test data
-        bucket_name = "test-bucket"
-        object_name = "test-object"
-        file_path = Path("test-file.txt")
-
+    def test_get_object(self) -> None:
         # Test successful get
         self.mock_client.bucket_exists.return_value = True
         self.mock_client.fget_object.return_value = MagicMock(
@@ -42,25 +48,14 @@ class TestMinioClient(unittest.TestCase):
             bucket_name, object_name, file_path)
         self.assertEqual(result.object_name, object_name)
 
-
-    def test_remove_object(self):
-        # Set up test data
-        bucket_name = "test-bucket"
-        object_name = "test-object"
-
+    def test_remove_object(self) -> None:
         # Test successful removal
         self.mock_client.bucket_exists.return_value = True
         self.client.remove_object(bucket_name, object_name)
         self.mock_client.remove_object.assert_called_once_with(
             bucket_name, object_name)
 
-
-    def test_update_object(self):
-        # Set up test data
-        bucket_name = "test-bucket"
-        object_name = "test-object"
-        file_path = Path("test-file.txt")
-
+    def test_update_object(self) -> None:
         # Test successful update
         self.mock_client.bucket_exists.return_value = True
         self.client.update_object(bucket_name, object_name, file_path)
@@ -68,6 +63,7 @@ class TestMinioClient(unittest.TestCase):
             bucket_name, object_name)
         self.mock_client.fput_object.assert_called_once_with(
             bucket_name, object_name, file_path)
+
 
 if __name__ == '__main__':
     unittest.main()
