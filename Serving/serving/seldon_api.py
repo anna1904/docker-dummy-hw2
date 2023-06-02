9212ad16aca3602b6581c357f4342286c2c18109
Anna Konovalenko <annakonovlnko@gmail.com>
2023-06-02 18:08:30 +0200

    Seldon init (todo image to upload)

--- Serving/serving/seldon_api.py
+++ Serving/serving/seldon_api.py
@@ -2,6 +2,7 @@ import logging
 from typing import List
 
 from serving.predictor import Predictor
+from PIL import Image
 
 logger = logging.getLogger()
 
@@ -11,6 +12,7 @@ class SeldonAPI:
         self.predictor = Predictor.default_from_model_registry()
 
     def predict(self, image):
-        results = self.predictor.predict(image)
+        img = Image.open(image)
+        results = self.predictor.predict(img)
         logger.info(results)
         return results
