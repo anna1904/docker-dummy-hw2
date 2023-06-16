267fe443d84e8e03ef68bd2014cb1ea31c035c19
Anna Konovalenko <annakonovlnko@gmail.com>
2023-06-17 01:05:23 +0200

    locustfile fix request

--- Serving/load-testing/locustfile.py
+++ Serving/load-testing/locustfile.py
@@ -5,20 +5,24 @@ from pathlib import Path
 from locust import HttpUser, between, task
 
 images_pool = [
-    "tests/assets/img_10.png",
-    "tests/assets/img_10.png",
+    "tests/assets/img_10.jpeg",
+    "tests/assets/img_10.jpeg",
 ]
 
 
 class PredictUser(HttpUser):
-    wait_time = between(1, 5)
+    wait_time = between(1, 50)
 
     @task
     def predict(self):
-        image_path = Path(os.getcwd()) / random.choice(images_pool)
+        image_path = random.choice(images_pool)
         with open(image_path, 'rb') as image_file:
             response = self.client.post(
                 "/predict",
-                files={"image_file": ("filename", image_file, "image/jpeg")},
+                files={'image': image_file},
                 headers={"accept": "application/json"}
             )
+
+
+a = PredictUser(HttpUser)
+print(a.predict())
